8e139e98ede7e4795571d043e9acb0041426361e
Samanta Torrez <dsamantatorrez@gmail.com>
2018-11-12 20:04:24 -0300

    Agregar: solucion MediaType error

--- src/com/cacic/controller/RevisionController.java
+++ src/com/cacic/controller/RevisionController.java
@@ -1,7 +1,12 @@
 package com.cacic.controller;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+import org.eclipse.persistence.oxm.MediaType;
 
 
 @Path("/revisiones")
@@ -10,6 +15,12 @@ public class RevisionController {
 	@GET
 	public String getRevision() {
 		return "prueba";
+	}
+	
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	public void altaRevision () {
+		
 	}
 	
 	
